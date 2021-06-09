bc0393d5d58c8b10c026fb9aec8cfbf0ea8e2450
Itai Y. Efrat <itai3397@gmail.com>
2021-06-09 18:14:36 +0300

    selectrum: README changes, linting, TODO overhaul

--- modules/completion/selectrum/config.el
+++ modules/completion/selectrum/config.el
@@ -27,7 +27,7 @@
                      (expand-file-name new-path))))))
       (call-interactively 'backward-delete-char)))
   (map! :map selectrum-minibuffer-map
-        [backspace]         #'+selectrum/backward-updir))
+        [backspace] #'+selectrum/backward-updir))
 
 (use-package! selectrum-prescient
   :when (featurep! +prescient)
@@ -165,7 +165,7 @@
   :init
   (map! :map minibuffer-local-map
         :desc "Cycle marginalia views"
-        "M-A"               #'marginalia-cycle)
+        "M-A" #'marginalia-cycle)
   :config
   (nconc marginalia-command-categories
          '((persp-switch-to-buffer . buffer)
