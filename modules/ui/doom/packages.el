f7247894f80470c164851b0a840380d6f1b0e95d
Itai Y. Efrat <itai3397@gmail.com>
2021-07-01 17:36:34 +0300

    DELETE LATER rollback solaire-mode DELETE LATER
    a recent commit of solaire-mode causes doom to crash when switching
    themes (when using emacs 28), but i want to rebase so we'll just do
    this untill it gets fixed.

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -2,4 +2,4 @@
 ;;; ui/doom/packages.el
 
 (package! doom-themes :pin "b2c0ea0f0778a6ea85b87e1b87916572e98e1fe7")
-(package! solaire-mode :pin "2298fd806d28daf511eb7836b23775e7df1f65e2")
+(package! solaire-mode :pin "186f97d7dddc85131fb1e7be5f89de226f26ae51")
