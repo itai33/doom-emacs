02d113da9f04ca27dbd1126464888c4c62659cb7
Itai Y. Efrat <itai3397@gmail.com>
2021-07-02 15:30:09 +0300

    Bump doom-themes
    hlissner/emacs-doom-themes@b2c0ea0 -> hlissner/emacs-doom-themes@ac359fc
    
    Bumped early in order to get better coloring for `vertico-mode`

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "b2c0ea0f0778a6ea85b87e1b87916572e98e1fe7")
+(package! doom-themes :pin "ac359fcc85b1bf8669f99263096affbcaab8e1a8")
 (package! solaire-mode :pin "186f97d7dddc85131fb1e7be5f89de226f26ae51")
